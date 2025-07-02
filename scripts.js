9e982c382de33b2c249b5cd63a26ff625f22ee39
vati347 <104317860+vati347@users.noreply.github.com>
2025-07-02 17:24:49 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,12 +6,12 @@ $(document).ready(function () {
         $(this).closest(".services-list-element").toggleClass("opened");
 
 
-        // If the section is now visible, scroll to it
-        if ($(this).next(".hiddensection").hasClass("shown")) {
-            setTimeout(() => {
-                $(this).next(".hiddensection")[0].scrollIntoView({ behavior: "smooth", block: "start" });
-            }, 300);
-        }
+        // // If the section is now visible, scroll to it
+        // if ($(this).next(".hiddensection").hasClass("shown")) {
+        //     setTimeout(() => {
+        //         $(this).next(".hiddensection")[0].scrollIntoView({ behavior: "smooth", block: "start" });
+        //     }, 300);
+        // }
     });
 
     $("#menu").click(function () {
