eb4858aae9592df3e1d1121ab88947bdc0b7563e
vati347 <104317860+vati347@users.noreply.github.com>
2025-07-15 11:15:27 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
 
-    $(".service").click(function () {
-        $(this).toggleClass("expand underline");
-        $(this).next(".hiddensection").toggleClass("shown");
-        $(this).closest(".services-list-element").toggleClass("opened");
+    // $(".service").click(function () {
+    //     $(this).toggleClass("expand underline");
+    //     $(this).next(".hiddensection").toggleClass("shown");
+    //     $(this).closest(".services-list-element").toggleClass("opened");
 
 
-        // // If the section is now visible, scroll to it
-        // if ($(this).next(".hiddensection").hasClass("shown")) {
-        //     setTimeout(() => {
-        //         $(this).next(".hiddensection")[0].scrollIntoView({ behavior: "smooth", block: "start" });
-        //     }, 300);
-        // }
-    });
+    //     // // If the section is now visible, scroll to it
+    //     // if ($(this).next(".hiddensection").hasClass("shown")) {
+    //     //     setTimeout(() => {
+    //     //         $(this).next(".hiddensection")[0].scrollIntoView({ behavior: "smooth", block: "start" });
+    //     //     }, 300);
+    //     // }
+    // });
 
     $("#menu").click(function () {
         $(this).toggleClass("expanded");
